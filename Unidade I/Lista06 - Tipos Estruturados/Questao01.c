ef930b33f0f25503d649e625a6b2c9247f7ba043
kaychenderson <146909378+kaychenderson@users.noreply.github.com>
2024-07-30 23:37:04 -0300

    update

--- Unidade I/Lista06 - Tipos Estruturados/Questao01.c
+++ Unidade I/Lista06 - Tipos Estruturados/Questao01.c
@@ -4,17 +4,15 @@
 #define MAX_TURMAS 10
 #define MAX_VAGAS 3
 
-typedef struct aluno{
+typedef struct aluno {
     int mat;
     char nome[81];
-    float nota1;
-    float nota2;
-    float nota3;
+    float notas[3];
     float media;
 } Aluno;
 
-typedef struct turma{
-    char id; 
+typedef struct turma {
+    char id;
     int vagas;
     Aluno* alunos[MAX_VAGAS];
 } Turma;
@@ -39,10 +37,10 @@ void matricularAluno(Turma* turma, int mat, char* nome) {
         for (index = 0; nome[index] != '\0'; index++) {
             novoAluno->nome[index] = nome[index];
         }
-        novoAluno->nome[index] = '\0'; 
-        novoAluno->nota1 = 0;
-        novoAluno->nota2 = 0;
-        novoAluno->nota3 = 0;
+        novoAluno->nome[index] = '\0';
+        for (int i = 0; i < 3; i++) {
+            novoAluno->notas[i] = 0;
+        }
         novoAluno->media = 0;
         
         for (index = 0; index < MAX_VAGAS; index++) {
@@ -61,13 +59,13 @@ void lancarNotas(Turma* turma) {
     for (int index = 0; index < MAX_VAGAS; index++) {
         if (turma->alunos[index] != NULL) {
             printf("Matricula: %d Aluno: %s\n", turma->alunos[index]->mat, turma->alunos[index]->nome);
-            printf("Digite a nota 1: ");
-            scanf("%f", &(turma->alunos[index]->nota1));
-            printf("Digite a nota 2: ");
-            scanf("%f", &(turma->alunos[index]->nota2));
-            printf("Digite a nota 3: ");
-            scanf("%f", &(turma->alunos[index]->nota3));
-            turma->alunos[index]->media = (turma->alunos[index]->nota1 + turma->alunos[index]->nota2 + turma->alunos[index]->nota3) / 3;
+            float soma = 0;
+            for (int i = 0; i < 3; i++) {
+                printf("Digite a nota %d: ", i + 1);
+                scanf("%f", &(turma->alunos[index]->notas[i]));
+                soma += turma->alunos[index]->notas[i];
+            }
+            turma->alunos[index]->media = soma / 3;
         }
     }
 }
@@ -76,13 +74,13 @@ void imprimirAlunos(Turma* turma) {
     for (int index = 0; index < MAX_VAGAS; index++) {
         if (turma->alunos[index] != NULL) {
             printf("+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+\n");
-            printf("| Matricula: %d                                    |\n", turma->alunos[index]->mat);
-            printf("| Nome: %s                                        \n", turma->alunos[index]->nome);
+            printf("| Matricula: %d                                  |\n", turma->alunos[index]->mat);
+            printf("| Nome: %s                                       \n", turma->alunos[index]->nome);
             printf("|------------------------------------------------+\n");
-            printf("| Nota 1: %.2f                                    |\n", turma->alunos[index]->nota1);
-            printf("| Nota 2: %.2f                                    |\n", turma->alunos[index]->nota2);
-            printf("| Nota 3: %.2f                                    |\n", turma->alunos[index]->nota3);
-            printf("| Media: %.2f                                     |\n", turma->alunos[index]->media);
+            for (int i = 0; i < 3; i++) {
+                printf("| Nota %d: %.2f                                  |\n", i + 1, turma->alunos[index]->notas[i]);
+            }
+            printf("| Media: %.2f                                    |\n", turma->alunos[index]->media);
             printf("+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+\n");
         }
     }
@@ -155,7 +153,7 @@ int main() {
                 if (turma != NULL) {
                     printf("Digite a matricula: ");
                     scanf("%d", &mat);
-                    getchar(); 
+                    getchar();
                     printf("Digite o nome: ");
                     scanf(" %[^\n]", nome);
                     matricularAluno(turma, mat, nome);
@@ -194,7 +192,6 @@ int main() {
         }
     } while (opcao != 6);
 
-    // Liberando memória alocada
     for (int i = 0; i < numTurmas; i++) {
         for (int j = 0; j < MAX_VAGAS; j++) {
             if (turmas[i]->alunos[j] != NULL) {
